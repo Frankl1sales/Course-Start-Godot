96ffaee9fb63edf462fb07aff24b5a4cae83fe1a
Frankl1sales <franklinsales1711@gmail.com>
2025-05-05 22:17:10 -0300

    added adjusment to web site

--- assets-do-curso-comecando-na-godot-4-main/README.md
+++
@@ -1,1 +0,0 @@
-Assets utilizados no curso "Começando na Godot 4", disponível no meu canal do YouTube, "Desenvolvendo Jogos".
--- scenes/jogo/jogo.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-# Quando o jogo inicia
-func _ready():
-	# Certifique-se de que os nós estão na árvore antes de conectar sinais
-	
-	pass
